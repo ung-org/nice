aabf2e0f651292400d8683b41656cecab65033a5
Jakob Kaivo <jkk@ung.org>
2019-03-14 13:18:24 -0400

    call setlocale() for localized perror() messages; cleaner return at end

--- nice.c
+++ nice.c
@@ -1,13 +1,19 @@
 #define _XOPEN_SOURCE 700
 #include <errno.h>
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+#define NICE_UTILITY_NOT_FOUND		127
+#define NICE_UTILITY_NOT_INVOKED	126
 
 int main(int argc, char *argv[])
 {
 	int increment = 0;
 	int c;
+
+	setlocale(LC_ALL, "");
 
 	while ((c = getopt(argc, argv, "n:")) != -1) {
 		switch (c) {
@@ -30,5 +36,10 @@ int main(int argc, char *argv[])
 	execvp(argv[0], argv);
 
 	perror("nice");
-	return errno == ENOENT ? 127 : 126;
+
+	if (errno == ENOENT) {
+		return NICE_UTILITY_NOT_FOUND;
+	}
+
+	return NICE_UTILITY_NOT_INVOKED;
 }
